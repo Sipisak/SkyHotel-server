b413ef2793de0d48777843a954f9b9bcdbad6190
sipis <sipis121@gmail.com>
2024-01-27 11:27:11 +0100

    init

---
+++ src/main/java/com/sipis/flyfinds/FlyFindsApplication.java
@@ -0,0 +1,13 @@
+package com.sipis.flyfinds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyFindsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyFindsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sipis/flyfinds/FlyFindsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sipis.flyfinds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyFindsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
